21b00d9f470930c8971c94e5d938903a7b5211f4
Bilal Adam <[beezee1113@gmail.com]>
2025-02-13 10:14:04 -0800

    Added hello to cam mac

--- src/main/java/edu/sandiego/cs/comp305/Main.java
+++ src/main/java/edu/sandiego/cs/comp305/Main.java
@@ -6,6 +6,7 @@ public class Main {
         System.out.println("Hello, Dr. Krause-Levy!");
         System.out.println("Hello, Dr. Olsen");
         System.out.println("Hello, Bilal");
+        System.out.println("Hello, Cam Mac");
 
     }
 }
