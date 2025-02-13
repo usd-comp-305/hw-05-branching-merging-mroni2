4c2f96075b33d596a92e883326646a8aee7f04f1
Dean Repetti <drepetti@sandiego.edu>
2025-02-13 10:11:39 -0800

    Changed to Kanye

--- src/main/java/edu/sandiego/cs/comp305/Main.java
+++ src/main/java/edu/sandiego/cs/comp305/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello, Class!");
-        System.out.println("Hello, Dean");
+        System.out.println("Hello, Kanye");
 
     }
 }
