f0d30a9ced273ec13b2b3af2d2f8fc78c9be8d6c
Jennifer Olsen <jenniferolsen@sandiego.edu>
2025-02-12 23:15:33 -0800

    feat: added a hello to Dr. Olsen

--- src/main/java/edu/sandiego/cs/comp305/Main.java
+++ src/main/java/edu/sandiego/cs/comp305/Main.java
@@ -2,6 +2,9 @@ package edu.sandiego.cs.comp305;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, Dr. Krause-Levy");
+
+        System.out.println("Hello, Class!");
+        System.out.println("Hello, Dr. Olsen");
+
     }
 }
