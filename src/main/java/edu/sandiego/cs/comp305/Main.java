fbde57ba80dd665ebb95f8c85f84a18754b225b5
Dean Repetti <drepetti@sandiego.edu>
2025-02-18 09:35:27 -0800

    Added Hello Lines

--- src/main/java/edu/sandiego/cs/comp305/Main.java
+++ src/main/java/edu/sandiego/cs/comp305/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello, Class!");
+        System.out.println("Hello, Dr. Krause-Levy");
         System.out.println("Hello, Kanye");
 
     }
