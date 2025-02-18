8df11071cf005cd5cc1d119b1b5bb374f7fc238b
Michael Rosalia <mrosalia@sandiego.edu>
2025-02-18 09:36:50 -0800

    feature: Added Diegos name to my new branch

--- src/main/java/edu/sandiego/cs/comp305/Main.java
+++ src/main/java/edu/sandiego/cs/comp305/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello, Dr. Krause-Levy!");
-        System.out.println("Hello, Michael Rosalia");
+        System.out.println("Hello, Diego Hook");
         System.out.println("Adding lines to help make the effect needed!");
 
     }
