18d6108e41556695a15d2e7cace0a12d4fa65b7c
Max-Aber <maximo@babun.com>
2025-02-18 09:36:47 -0800

    added print in main with my name

--- src/main/java/edu/sandiego/cs/comp305/Main.java
+++ src/main/java/edu/sandiego/cs/comp305/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
 
             System.out.println("Hello, Dr. Krause-Levy");
-            System.out.println("Hello, Maximo");
+            System.out.println("Hello Maximo");
 
     }
 }
