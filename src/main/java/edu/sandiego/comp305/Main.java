c178f46b5047dd90859a25c16d4dcb83b963f20e
Sophia Krause-Levy <skrauselevy@sandiego.edu>
2026-02-05 16:22:09 -0800

    back to original

--- src/main/java/edu/sandiego/comp305/Main.java
+++ src/main/java/edu/sandiego/comp305/Main.java
@@ -4,6 +4,5 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello, Dr. Krause-Levy!");
-        System.out.println("Hello, Dr. Krause-Levy!");
     }
 }
