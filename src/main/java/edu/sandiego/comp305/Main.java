27a5c9153c486c6258a32b39293fcfa7db57f491
Jennifer Olsen <jenniferolsen@sandiego.edu>
2026-02-05 16:16:09 -0800

    feat: added Jenny name

--- src/main/java/edu/sandiego/comp305/Main.java
+++ src/main/java/edu/sandiego/comp305/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello, Dr. Krause-Levy!");
-        System.out.println("Hello, Jenny");
+        System.out.println("Hello, Jenn");
+
     }
 }
