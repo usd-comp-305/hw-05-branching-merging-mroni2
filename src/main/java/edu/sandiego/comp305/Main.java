5e172746f5d07dda4c9212b8870ed94ff8da098b
Tammy Mathews <tmathews@sandiego.edu>
2026-02-09 10:15:29 -0800

    added hello print statement

--- src/main/java/edu/sandiego/comp305/Main.java
+++ src/main/java/edu/sandiego/comp305/Main.java
@@ -4,6 +4,6 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello, Dr. Krause-Levy!");
-        System.out.println(" Hello, Devon Montgomery-Eder!");
+        System.out.println(" Hello, Tammy!");
     }
 }
