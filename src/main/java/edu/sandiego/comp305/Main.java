227d6baf07364dcd3b403e1b96e9995acf561f3c
Aiden Domingo <adomingo@sandiego.edu>
2026-02-09 10:12:50 -0800

    Modified printed output in main method

--- src/main/java/edu/sandiego/comp305/Main.java
+++ src/main/java/edu/sandiego/comp305/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello, Dr. Krause-Levy! Hello, Aiden Domingo");
+        System.out.println("Hello, other people!");
     }
 }
