afae583f70e32b080b86c46ec1884201a252969d
Isaac <fajardoisaac21@gmail.com>
2026-02-09 10:08:37 -0800

    fix: Changed name in print in main

--- src/main/java/edu/sandiego/comp305/Main.java
+++ src/main/java/edu/sandiego/comp305/Main.java
@@ -3,7 +3,7 @@ package edu.sandiego.comp305;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello, Dr. Krause-Levy!");
+        System.out.println("Hello, Isaac Fajardo!");
         System.out.println(" Hello, Devon Montgomery-Eder!");
     }
 }
