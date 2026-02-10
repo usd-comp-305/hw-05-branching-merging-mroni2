6da4e31983956da3d4051a7959dacbd29a09a1ca
Jet Tang Ro Starkweather <jstarkweather@sandiego.edu>
2026-02-10 11:33:34 -0800

    Add "Hello, Jet Tang Ro Starkweather" to print in the main method

--- src/main/java/edu/sandiego/comp305/Main.java
+++ src/main/java/edu/sandiego/comp305/Main.java
@@ -2,6 +2,6 @@ package edu.sandiego.comp305;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, Dr. Krause-Levy! Hello, Jet Tang Ro Starkweahter!");
+        System.out.println("Hello, Dr. Krause-Levy! \n Hello, Jet Tang Ro Starkweahter!");
     }
 }
