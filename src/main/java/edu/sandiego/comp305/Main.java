9e1409499cbb6ba00cd034186ac370c28d92f9b8
devongalb <dgalbraith@sandiego.edu>
2026-02-10 11:34:59 -0800

    feat: changed name outprint in main to agent smith

--- src/main/java/edu/sandiego/comp305/Main.java
+++ src/main/java/edu/sandiego/comp305/Main.java
@@ -2,6 +2,6 @@ package edu.sandiego.comp305;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, Devon");
+        System.out.println("Hello, Agent Smith");
     }
 }
