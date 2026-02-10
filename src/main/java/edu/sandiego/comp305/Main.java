fe239918f99e5576d0a20a29d45467895e1e9f51
joeym <joeymats4@icloud.com>
2026-02-10 11:39:15 -0800

    update in main to print Hello, Brett.

--- src/main/java/edu/sandiego/comp305/Main.java
+++ src/main/java/edu/sandiego/comp305/Main.java
@@ -3,5 +3,6 @@ package edu.sandiego.comp305;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, Joe Matschina");
+        System.out.println("Hello, Brett");
     }
 }
