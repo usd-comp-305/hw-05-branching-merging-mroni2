f9bc7cd3a544ae719b5f47d833e249d41d03de22
Penny Yanez <pyanez@sandiego.edu>
2026-02-10 11:37:54 -0800

    added new print statement to main method

--- src/main/java/edu/sandiego/comp305/Main.java
+++ src/main/java/edu/sandiego/comp305/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, Dr. Krause-Levy!");
         System.out.println("Hello, Penny");
+        System.out.println("Hello, Callie");
     }
 }
