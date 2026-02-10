887da38bac0f7c765faba40027b294e7c115f215
Santiago Guerrero <santiguerrero@sandiego.edu>
2026-02-10 11:32:58 -0800

    Main update: added hello message to Mr. Sir

--- src/main/java/edu/sandiego/comp305/Main.java
+++ src/main/java/edu/sandiego/comp305/Main.java
@@ -3,5 +3,7 @@ package edu.sandiego.comp305;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, Dr. Krause-Levy!");
+        System.out.println("Hello, Santi");
+        System.out.println("Hello, Mr. Sir");
     }
 }
