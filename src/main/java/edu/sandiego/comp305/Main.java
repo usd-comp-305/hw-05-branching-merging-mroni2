979e110e2e28070cc17aed18a84780ece39cfad8
devongalb <dgalbraith@sandiego.edu>
2026-02-10 11:59:01 -0800

    corrected the print lines

--- src/main/java/edu/sandiego/comp305/Main.java
+++ src/main/java/edu/sandiego/comp305/Main.java
@@ -2,6 +2,8 @@ package edu.sandiego.comp305;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Hello Devon");
         System.out.println("Hello, Agent Smith");
+
     }
 }
