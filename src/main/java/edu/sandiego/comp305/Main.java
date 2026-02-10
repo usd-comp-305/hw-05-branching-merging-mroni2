d745a23e65e06b3a03cd73664331dc2238863710
Ian Falkenstrom <ifalkenstrom@sandiego.edu>
2026-02-10 11:54:46 -0800

    added new print statement

--- src/main/java/edu/sandiego/comp305/Main.java
+++ src/main/java/edu/sandiego/comp305/Main.java
@@ -4,5 +4,11 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, Ian!");
         System.out.println("Hello, Dr. Olsen!");
-    }
+        System.out.println("Hello, Jacob");
 }
+}
+
+
+
+
+
