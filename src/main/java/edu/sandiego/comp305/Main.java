436744567f318275c0ecfd01db90b44def34192c
Coco <csequeira@sandiego.edu>
2026-02-10 11:31:30 -0800

    feat changed main method print statement

--- src/main/java/edu/sandiego/comp305/Main.java
+++ src/main/java/edu/sandiego/comp305/Main.java
@@ -2,7 +2,7 @@ package edu.sandiego.comp305;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, Dr. Krause-Levy!");
+        System.out.println("Hello, Coco!");
         System.out.println("Hello, Dr. Olsen!");
     }
 }
