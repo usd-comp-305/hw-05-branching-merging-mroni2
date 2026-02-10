0ff830d9d108a515c2a86b406822006a8548da76
Coco <csequeira@sandiego.edu>
2026-02-10 11:54:08 -0800

    <WIP> added main method print statement

--- src/main/java/edu/sandiego/comp305/Main.java
+++ src/main/java/edu/sandiego/comp305/Main.java
@@ -2,7 +2,8 @@ package edu.sandiego.comp305;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, Person!");
+        System.out.println("Hello, Coco!");
+        System.out.println("Hello, Abigail");
         System.out.println("Hello, Dr. Olsen!");
     }
 }
