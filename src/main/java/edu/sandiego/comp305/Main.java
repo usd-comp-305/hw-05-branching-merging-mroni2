e54cee6ba8e055b564d7211db5fd9dee6c3ec227
kparadis1 <kparadis@sandiego.edu>
2026-02-10 11:34:17 -0800

    feature: added name

--- src/main/java/edu/sandiego/comp305/Main.java
+++ src/main/java/edu/sandiego/comp305/Main.java
@@ -3,5 +3,6 @@ package edu.sandiego.comp305;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, Dr. Krause-Levy!");
+        System.out.println("Hello, Khy Paradis!");
     }
 }
