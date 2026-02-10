9c545af512edcc8b94b9c4421290b4969cfaf18c
Sophia Krause-Levy <skrauselevy@sandiego.edu>
2026-02-09 17:37:41 -0800

    reset for lect

--- src/main/java/edu/sandiego/comp305/Main.java
+++ src/main/java/edu/sandiego/comp305/Main.java
@@ -2,9 +2,6 @@ package edu.sandiego.comp305;
 
 public class Main {
     public static void main(String[] args) {
-
-        System.out.println("Hello, Dr. Krause-Levy! Hello, Aiden Domingo");
         System.out.println("Hello, Dr. Krause-Levy!");
-        System.out.println("Hello, Zahria Harris-Cole!");
     }
 }
